c69c2b3fd527e242e44ddc5f95f5191860819e5e
AndrewYeh1 <andrewyeh08299@gmail.com>
2024-10-05 16:03:20 -0700

    pull data from star api

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 def main():
     pass
+=======
+import coords.planetData
+import gui.gui
+import coords.starData
+
+
+def main():
+    # exoplanets = coords.planetData.PlanetData("resources/exoplanets.xlsx")
+    # win = gui.gui.Gui()
+    stars = coords.starData.StarData()
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
